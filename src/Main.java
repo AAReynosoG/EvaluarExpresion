ff72625af75f3d3b570f762e5f52ffda4d137e85
Alonso <abrahamreynoso64@gmail.com>
2023-09-28 11:28:52 -0600

    Cambios

--- src/Main.java
+++ src/Main.java
@@ -44,34 +44,51 @@ public class Main {
     }
 
     public static void EvaluarCadena(String cadena) {
-        char[] cadenaChar = cadena.toUpperCase().toCharArray();
+        char[] cadenaChar = cadena.toCharArray();
         System.out.println("La cadena ingresada: " + cadena);
 
 
-        char[] vocales = {'A', 'E', 'I', 'O', 'U'};
-        char[] consonantes = {'B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z'};
+        char[] vocales = {'a', 'e', 'i', 'o', 'u'};
+        char[] consonantes = {'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'};
         char[] DigInicial = {'2', '6'};
         char[] Digitos = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
         char[] DigFinal = {'3', '9'};
-
+        ///
         if (cadenaChar.length > 0) {
             char primerCaracter = cadenaChar[0];
             boolean esVocal = false;
+            int i = 0;
 
             for (char vocal : vocales) {
                 if (primerCaracter == vocal) {
                     esVocal = true;
+                    i = i+1;
                     break;
                 }
             }
-
+            ///
             if (esVocal) {
-                System.out.println("El primer carácter es una vocal.");
+                ///
+                if (cadenaChar.length > 1) {
+                    char segundoChar = cadenaChar[i];
+                    boolean esDig = false;
+
+                    for (char digIn : DigInicial) {
+                        if (primerCaracter == digIn) {
+                            esDig = true;
+                            i = i + 1;
+                            break;
+                        }
+                    }
+                    ///
+                }else {
+
+                }
             } else {
-                System.out.println("El primer carácter no es una vocal.");
+                System.out.println("");
             }
         } else {
-            System.out.println("La cadena está vacía.");
+            System.out.println("No se ingreso ninguna cadena");
         }
 
 
