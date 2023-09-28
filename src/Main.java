0728c84df5d517410239bcf837b5f0c675e357dc
Alonso <abrahamreynoso64@gmail.com>
2023-09-28 12:33:11 -0600

    Cambios

--- src/Main.java
+++ src/Main.java
@@ -45,48 +45,47 @@ public class Main {
 
     public static void EvaluarCadena(String cadena) {
         char[] cadenaChar = cadena.toCharArray();
-        System.out.println("La cadena ingresada: " + cadena);
-
 
         char[] vocales = {'a', 'e', 'i', 'o', 'u'};
         char[] consonantes = {'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'};
         char[] DigInicial = {'2', '6'};
         char[] Digitos = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
         char[] DigFinal = {'3', '9'};
+        int i = 0;
         ///
-        if (cadenaChar.length > 0) {
+        if (cadenaChar.length > i) {
             char primerCaracter = cadenaChar[0];
             boolean esVocal = false;
-            int i = 0;
 
             for (char vocal : vocales) {
                 if (primerCaracter == vocal) {
-                    esVocal = true;
                     i = i+1;
+                    esVocal = true;
                     break;
                 }
             }
-            ///
             if (esVocal) {
-                ///
-                if (cadenaChar.length > 1) {
-                    char segundoChar = cadenaChar[i];
-                    boolean esDig = false;
 
-                    for (char digIn : DigInicial) {
-                        if (primerCaracter == digIn) {
-                            esDig = true;
-                            i = i + 1;
-                            break;
-                        }
-                    }
-                    ///
-                }else {
-
-                }
             } else {
-                System.out.println("");
+                char segundoCaracter = cadenaChar[i];
+                boolean esConsonante = false;
+                for (char cons : consonantes) {
+                    if (primerCaracter == cons && cadenaChar.length == 2){
+                        for (char vocal : vocales) {
+                            if (segundoCaracter == vocal) {
+                                System.out.println("La cadena: " + cadena + "es valida");
+                            }
+                            else {
+                                System.out.println("La cadena es invalida");
+                            }
+                        }
+                    }else {
+                        System.out.println("La cadena ingresada: " + cadena + "es invalida");
+                    }
+                    break;
+                }
             }
+
         } else {
             System.out.println("No se ingreso ninguna cadena");
         }
