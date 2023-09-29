ad59d6e0c81aaf379f7ec3dee97c7a2d52e7513d
Alonso <abrahamreynoso64@gmail.com>
2023-09-28 20:17:44 -0600

    Ya valida el camino de las vocales, falta validar que puedan ser mas de 4 numeros

--- src/Main.java
+++ src/Main.java
@@ -51,7 +51,7 @@ public class Main {
         char[] DigInicial = {'2', '6'};
         char[] Digitos = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
         char[] DigFinal = {'3', '9'};
-        int i = 0;
+        int i = 1;
 
         if (cadenaChar.length > 1) {
             char primerCaracter = cadenaChar[0];
@@ -76,7 +76,35 @@ public class Main {
                     System.out.println("La cadena: " + "'" + cadena + "'" + " es valida");
                 }
                 else {
-                    
+                    if (!esCons){
+
+                        if (cadenaChar.length >= 5 && esVocal(cadenaChar[0])) {
+                            if (esCaracterEnArreglo(cadenaChar[i], DigInicial)) {
+                                i++;
+                                int digitCount = 0;
+                                while (i < cadenaChar.length && digitCount < 2) {
+                                    if (esCaracterEnArreglo(cadenaChar[i], Digitos)) {
+                                        digitCount++;
+                                    } else {
+                                        break;
+                                    }
+                                    i++;
+                                }
+
+                                if (digitCount >= 2 && i < cadenaChar.length && esCaracterEnArreglo(cadenaChar[i], DigFinal)) {
+                                    i++;
+                                    if (i < cadenaChar.length && esCaracterEnArreglo(cadenaChar[i], consonantes)) {
+                                        System.out.println("La cadena: '" + cadena + "' es válida");
+                                        return;
+                                    }
+                                }
+                            }
+                        }
+
+                        System.out.println("La cadena: '" + cadena + "' es inválida");
+
+
+                    }
                 }
             } else {
                 System.out.println("lol");
@@ -86,6 +114,24 @@ public class Main {
             System.out.println("No se ingreso ninguna cadena o la cadena es demasiado corta!");
         }
 
+    }
 
+    public static boolean esVocal(char c) {
+        char[] vocales = {'a', 'e', 'i', 'o', 'u'};
+        for (char vocal : vocales) {
+            if (c == vocal) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean esCaracterEnArreglo(char c, char[] arreglo) {
+        for (char elemento : arreglo) {
+            if (c == elemento) {
+                return true;
+            }
+        }
+        return false;
     }
 }
