95f96060e41282d8a655b707673f580f4b5e8831
Alonso <abrahamreynoso64@gmail.com>
2023-10-06 07:19:23 -0600

    Final

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
         String[] creditos = { "1.- Carlos Gabriel Romero", "2.- Anthony Fuentes",
                 "3.- Abraham Alonso Reynoso Gonzalez - 22170060" };
-        int opcion;
+        String opcion;
         String cadena = "";
 
         do {
@@ -15,31 +15,38 @@ public class Main {
             System.out.println("2.  Creditos.");
             System.out.println("3.  Salir.");
 
-            opcion = sc.nextInt();
+            opcion = sc.nextLine();
 
             switch (opcion) {
-                case 1:
+                case "1":
                     System.out.println("Ingresa la cadena que deseas validar:");
-                    sc.nextLine();
                     cadena = sc.nextLine();
                     EvaluarCadena(cadena);
+                    System.out.println("Presiona 'Enter' para continuar...");
+                    sc.nextLine();
                     break;
-                case 2:
+                case "2":
                     System.out.println("--CREDITOS--:");
-                    for (String str : creditos) {
-                        System.out.println(str);
-                    }
+                    System.out.println("## Estructura de Datos Aplicadas ##");
+                    System.out.println("Trabajo realizado por:");
+                    System.out.println("1.- Carlos Gabriel Romero Garcia.- 22170145 ");
+                    System.out.println("2.- Anthony Fuentes Carrera.- 22170187 ");
+                    System.out.println("3.- Abraham Alonso Reynoso Gonzalez.- 22170060");
                     System.out.println("--########--:");
+                    System.out.println("Presiona 'Enter' para continuar...");
+                    sc.nextLine();
                     break;
-                case 3:
+                case "3":
                     System.out.println("Hasta luego...!");
                     break;
                 default:
                     System.out.println("Opción no válida.");
+                    System.out.println("Presiona 'Enter' para continuar...");
+                    sc.nextLine();
                     break;
             }
 
-        } while (opcion != 3);
+        } while (!opcion.equals("3"));
 
         sc.close();
     }
@@ -58,90 +65,92 @@ public class Main {
 
         if(sinMayus){
 
+            if (cadenaChar.length > 1) {
+                char ultCaracter = cadenaChar[cadenaChar.length - 1];
+                char primerCaracter = cadenaChar[0];
+                boolean esVocal = false;
+                boolean esConsonante = false;
+
+
+                for (char vocal : vocales) {
+                    if (primerCaracter == vocal) {
+                        esVocal = true;
+                        break;
+                    }
+                }
+                for (char consonante : consonantes) {
+                    if (primerCaracter == consonante) {
+                        esConsonante = true;
+                        break;
+                    }
+                }
+
+                if(esVocal == false && esConsonante == false){
+                    System.out.println("Inicia con consonante o vocal");
+                }
+
+                if (esVocal) {
+                    if (esConsonante(ultCaracter)) {
+                        if (cadenaChar.length == 2) {
+                            System.out.println("La cadena: '" + cadena + "' es valida");
+                        } else if (cadenaChar.length > 2) {
+                            char segundoCaracter = cadenaChar[1];
+                            if (segundoCaracter == '2' || segundoCaracter == '6') {
+                                int ii = 2;
+                                int countDigits = 0;
+                                while (ii < cadenaChar.length && esCaracterEnArreglo(cadenaChar[ii], Digitos)) {
+                                    countDigits++;
+                                    ii++;
+                                }
+                                if (countDigits >= 3 && (cadenaChar[ii - 1] == '3' || cadenaChar[ii - 1] == '9')) {
+                                    System.out.println("La cadena: '" + cadena + "' es valida");
+                                } else {
+                                    System.out.println("La cadena no cumple con el formato de dígitos requerido");
+                                }
+                            } else {
+                                System.out.println("La cadena debe comenzar con un 2 o un 6 después de la consonante inicial");
+                            }
+                        }
+                    } else {
+                        System.out.println("Tienes que terminar con vocal");
+                    }
+                } else if (esConsonante) {
+                    if (esVocal(ultCaracter)) {
+                        if (cadenaChar.length == 2) {
+                            System.out.println("La cadena: '" + cadena + "' es valida");
+                        } else if (cadenaChar.length > 2) {
+                            char segundoCaracter = cadenaChar[1];
+                            if (segundoCaracter == '2' || segundoCaracter == '6') {
+                                int ii = 2;
+                                int countDigits = 0;
+                                while (ii < cadenaChar.length && esCaracterEnArreglo(cadenaChar[ii], Digitos)) {
+                                    countDigits++;
+                                    ii++;
+                                }
+                                if (countDigits >= 3 && (cadenaChar[ii - 1] == '3' || cadenaChar[ii - 1] == '9')) {
+                                    System.out.println("La cadena: '" + cadena + "' es valida");
+                                } else {
+                                    System.out.println("La cadena no cumple con el formato de dígitos requerido");
+                                }
+                            } else {
+                                System.out.println("La cadena debe comenzar con un 2 o 3 y terminar con un 6 o 9 después de la vocal inicial");
+                            }
+                        }
+                    } else {
+                        System.out.println("Tienes que terminar con consonante");
+                    }
+                }
+
+            } else {
+                System.out.println("No se ingreso ninguna cadena o la cadena es demasiado corta!");
+            }
+
         }
         else{
             System.out.println("La cadena no debe tener mayusculas!");
         }
 
-        if (cadenaChar.length > 1) {
-            char ultCaracter = cadenaChar[cadenaChar.length - 1];
-            char primerCaracter = cadenaChar[0];
-            boolean esVocal = false;
-            boolean esConsonante = false;
 
-
-            for (char vocal : vocales) {
-                if (primerCaracter == vocal) {
-                    esVocal = true;
-                    break;
-                }
-            }
-            for (char consonante : consonantes) {
-                if (primerCaracter == consonante) {
-                    esConsonante = true;
-                    break;
-                }
-            }
-
-            if(esVocal == false && esConsonante == false){
-                System.out.println("Inicia con consonante o vocal");
-            }
-
-            if (esVocal) {
-                if (esConsonante(ultCaracter)) {
-                    if (cadenaChar.length == 2) {
-                        System.out.println("La cadena: '" + cadena + "' es valida");
-                    } else if (cadenaChar.length > 2) {
-                        char segundoCaracter = cadenaChar[1];
-                        if (segundoCaracter == '2' || segundoCaracter == '6') {
-                            int ii = 2; 
-                            int countDigits = 0;
-                            while (ii < cadenaChar.length && esCaracterEnArreglo(cadenaChar[ii], Digitos)) {
-                                countDigits++;
-                                ii++;
-                            }
-                            if (countDigits >= 3 && (cadenaChar[ii - 1] == '3' || cadenaChar[ii - 1] == '9')) {
-                                System.out.println("La cadena: '" + cadena + "' es valida");
-                            } else {
-                                System.out.println("La cadena no cumple con el formato de dígitos requerido");
-                            }
-                        } else {
-                            System.out.println("La cadena debe comenzar con un 2 o un 6 después de la consonante inicial");
-                        }
-                    }
-                } else {
-                    System.out.println("Tienes que terminar con vocal");
-                }
-            } else if (esConsonante) {
-                if (esVocal(ultCaracter)) {
-                    if (cadenaChar.length == 2) {
-                        System.out.println("La cadena: '" + cadena + "' es valida");
-                    } else if (cadenaChar.length > 2) {
-                        char segundoCaracter = cadenaChar[1];
-                        if (segundoCaracter == '2' || segundoCaracter == '6') {
-                            int ii = 2; 
-                            int countDigits = 0;
-                            while (ii < cadenaChar.length && esCaracterEnArreglo(cadenaChar[ii], Digitos)) {
-                                countDigits++;
-                                ii++;
-                            }
-                            if (countDigits >= 3 && (cadenaChar[ii - 1] == '3' || cadenaChar[ii - 1] == '9')) {
-                                System.out.println("La cadena: '" + cadena + "' es valida");
-                            } else {
-                                System.out.println("La cadena no cumple con el formato de dígitos requerido");
-                            }
-                        } else {
-                            System.out.println("La cadena debe comenzar con un 2 o 3 y terminar con un 6 o 9 después de la vocal inicial");
-                        }
-                    }
-                } else {
-                    System.out.println("Tienes que terminar con consonante");
-                }
-            }
-                    
-        } else {
-            System.out.println("No se ingreso ninguna cadena o la cadena es demasiado corta!");
-        }
 
     }
 
